f4380ef5b41af4f0befaab3054e0bffb6af4996c
Your Name <you@example.com>
2024-07-08 13:12:44 +0200

    #1

---
+++ acquisitions.js
@@ -0,0 +1,28 @@
+import { getAquisitionsByYear } from "./api.js";
+
+// ...
+
+const data = await getAquisitionsByYear();
+new Chart(document.getElementById("acquisitions"), {
+  type: "bar",
+  options: {
+    animation: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+  },
+  data: {
+    labels: data.map((row) => row.year),
+    datasets: [
+      {
+        label: "Acquisitions by year",
+        data: data.map((row) => row.count),
+      },
+    ],
+  },
+});
